e7fca3d5a31dfde28fc877014313a7cdf9a88497
Fikri <afikrik@yahoo.com>
2020-07-28 14:35:52 +1000

    Finish ch 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,59 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 26},
+      
+    ]
+  }
+
+  switchNameHandler = () => {
+    // setState merges the old and new states
+    this.setState( {
+      persons: [
+        {name: 'Maximilian', age: 28},
+        {name: 'Manu', age: 29},
+        {name: 'Stephanie', age: 27}
+      ]
+    })
+  }
+
+  nameChangeHandler = (event) => {
+    this.setState( {
+      persons: [
+        {name: 'Maximilian', age: 28},
+        {name: event.target.value, age: 29},
+        {name: 'Stephanie', age: 27}
+      ]
+    })
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Test</h1>
+        <button
+         style={style} 
+         onClick={this.switchNameHandler}>Switch person</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
+        changed={this.nameChangeHandler} >My Hobbies:Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
