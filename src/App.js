9c05c769643fa0f35ce68b952bc58817d2220b0b
Fikri <afikrik@yahoo.com>
2020-08-03 13:16:23 +1000

    added styledbutton

--- src/App.js
+++ src/App.js
@@ -3,6 +3,29 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
+// Use radium to put hover properties in style
+// Styleroot is used for enabling css media query as in Person.js
+// import Radium, { StyleRoot } from 'radium';
+
+// needed to do the css thing (style.button``)
+import styled from 'styled-components';
+
+
+// use props.alt defined when <StyledButton> element is made
+const StyledButton = styled.button`
+
+  background-color: ${props => props.alt ? 'red' : 'green'};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+  &:hover {
+    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+    color: black;
+  }
+`;
+
 class App extends Component {
   
   state = {
@@ -13,6 +36,8 @@ class App extends Component {
       
     ]
   }
+
+
 
 
   nameChangeHandler = (event, id) => {
@@ -41,22 +66,44 @@ class App extends Component {
     })
   }
 
-  deletePersonHandler = (personIndex) => {
+  deletePersonHandler = (personId) => {
     // Copy array (avoid mutating)
+
     const persons = this.state.persons.slice();
 
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === personId;
+    });
+    console.log(personIndex);
+
     persons.splice(personIndex,1);
-    this.setState(this.state.persons: persons);
+    this.setState({persons: persons});
   }
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
+
+    const classes = [];
+
+    if(this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+
+    if(this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
+
 
     let persons = null;
 
@@ -66,21 +113,29 @@ class App extends Component {
         <div>
             {this.state.persons.map((person) => {
               return <Person name={person.name} age={person.age} 
-              click={this.deletePersonHandler} key={person.id}
+              click={() => this.deletePersonHandler(person.id)} key={person.id}
               changed={(event) => this.nameChangeHandler(event, person.id)} />
             })}
         </div>
       )
+      style.backgroundColor = 'red';
+      style[':hover']= {
+        backgroundColor: 'salmon',
+        color: 'black'
+      };
     }
 
     return (
-      <div className="App">
-        <h1>Test</h1>
-        <button
-         style={style} 
-         onClick={this.togglePersonsHandler}>Switch person</button>
-        {persons}          
-      </div>
+      // to use styleroot, wrap in this element
+      // <StyleRoot>
+        <div className="App">
+          <h1>Test</h1>
+          <p className={classes.join(' ')}>This is really workin!</p>
+          <StyledButton onClick={this.togglePersonsHandler} alt={this.state.doesShow}>
+          Switch person</StyledButton>
+          {persons}          
+        </div>
+      // </StyleRoot>
     );
   }
 }
